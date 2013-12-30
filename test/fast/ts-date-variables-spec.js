e2c1e4e9eba6311e9120bb56fc4176fa6fed5f11
John Martin <curmudgeon@rallydev.com>
2013-12-30 09:52:27 -0800

    add today keyword

--- test/fast/ts-date-variables-spec.js
+++ test/fast/ts-date-variables-spec.js
@@ -1,6 +1,9 @@
 describe("When working with query strings",function(){
     var today_at_midnight = new Date(Ext.clone(new Date()).setHours(0,0,0,0));
+    var tomorrow_at_midnight = Rally.util.DateTime.add(today_at_midnight,"day",1);
+    
     var today_at_midnight_iso = Rally.util.DateTime.toIsoString(today_at_midnight).replace(/T.*$/,"");
+    var tomorrow_at_midnight_iso = Rally.util.DateTime.toIsoString(tomorrow_at_midnight).replace(/T.*$/,"");
         
     it("should replace 'today' with the date when operator is <",function(){
         var query_string = "( Iteration.StartDate < today )";
@@ -25,6 +28,13 @@ describe("When working with query strings",function(){
         var filter = Ext.create('TSStringFilter',{query_string:query_string});
         expect(filter.toString()).toEqual(clean_string);
     });
-    
+        
+    it("should replace 'today' with end of day when operator is <",function(){
+        var query_string = '( Iteration.StartDate < "today" )';
+       
+        var clean_string = '(Iteration.StartDate < "' + today_at_midnight_iso + '")';
+        var filter = Ext.create('TSStringFilter',{query_string:query_string});
+        expect(filter.toString()).toEqual(clean_string);
+    });
     
 });
