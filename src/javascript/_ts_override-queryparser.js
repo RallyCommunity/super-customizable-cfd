4831c92cefa3e97b026756e1801aa298403b6c1c
John Martin <curmudgeon@rallydev.com>
2013-12-30 10:06:40 -0800

    adjust for > or <

--- src/javascript/_ts_override-queryparser.js
+++ src/javascript/_ts_override-queryparser.js
@@ -29,7 +29,11 @@ Ext.override(Rally.data.util.QueryStringParser,{
         var xform_value = value;
         if ( operator != "AND" && operator != "OR" ) {
             if ( value == "today" ) {
-                xform_value = this._getIsoMidnight(new Date());
+                if ( operator == "<" ) {
+                    xform_value = this._getIsoMidnight(new Date());
+                } else if ( operator == ">" ) {
+                    xform_value = this._getIsoMidnight(Rally.util.DateTime.add(new Date(),"day",1));
+                }
             }
         } 
         return xform_value;
@@ -38,7 +42,7 @@ Ext.override(Rally.data.util.QueryStringParser,{
      * 
      */
     _getIsoMidnight: function(js_date) {
-        var js_at_midnight = new Date(Ext.clone(new Date()).setHours(0,0,0,0));
+        var js_at_midnight = new Date(Ext.clone(js_date).setHours(0,0,0,0));
         var iso_at_midnight = Rally.util.DateTime.toIsoString(js_at_midnight).replace(/T.*$/,"");
         return iso_at_midnight;
     }
--- test/fast/ts-date-variables-spec.js
+++ test/fast/ts-date-variables-spec.js
@@ -5,7 +5,7 @@ describe("When working with query strings",function(){
     var today_at_midnight_iso = Rally.util.DateTime.toIsoString(today_at_midnight).replace(/T.*$/,"");
     var tomorrow_at_midnight_iso = Rally.util.DateTime.toIsoString(tomorrow_at_midnight).replace(/T.*$/,"");
         
-    it("should replace 'today' with the date when operator is <",function(){
+    it("should replace 'today' with beginning of day when operator is <",function(){
         var query_string = "( Iteration.StartDate < today )";
        
         var clean_string = '(Iteration.StartDate < "' + today_at_midnight_iso + '")';
@@ -13,7 +13,7 @@ describe("When working with query strings",function(){
         expect(filter.toString()).toEqual(clean_string);
     });
     
-    it("should replace 'today' with the date when operator is < and today is in quotes",function(){
+    it("should replace 'today' with beginning of day when operator is < and today is in quotes",function(){
         var query_string = '( Iteration.StartDate < "today" )';
        
         var clean_string = '(Iteration.StartDate < "' + today_at_midnight_iso + '")';
@@ -21,7 +21,7 @@ describe("When working with query strings",function(){
         expect(filter.toString()).toEqual(clean_string);
     });
     
-    it("should replace 'today' with the date when operator is < in multiple nodes",function(){
+    it("should replace 'today' with beginning of day when operator is < in multiple nodes",function(){
         var query_string = '(((Iteration.StartDate < "today") AND (Iteration.StartDate < "today")) AND (Iteration.StartDate < "fred"))';
        
         var clean_string = '(((Iteration.StartDate < "' + today_at_midnight_iso + '") AND (Iteration.StartDate < "' + today_at_midnight_iso + '")) AND (Iteration.StartDate < "fred"))';
@@ -30,9 +30,9 @@ describe("When working with query strings",function(){
     });
         
     it("should replace 'today' with end of day when operator is <",function(){
-        var query_string = '( Iteration.StartDate < "today" )';
+        var query_string = '( Iteration.StartDate > "today" )';
        
-        var clean_string = '(Iteration.StartDate < "' + today_at_midnight_iso + '")';
+        var clean_string = '(Iteration.StartDate > "' + tomorrow_at_midnight_iso + '")';
         var filter = Ext.create('TSStringFilter',{query_string:query_string});
         expect(filter.toString()).toEqual(clean_string);
     });
